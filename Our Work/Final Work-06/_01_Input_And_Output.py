e96e5efcb6eec51448743e220f52a171b25f028e
Md shahadet Hossain <hossain.mdshahahdatca@gmail.com>
2023-03-25 14:03:39 -0600

    Python_6th

---
+++ Our Work/Final Work-06/_01_Input_And_Output.py
@@ -0,0 +1,49 @@
+
+# Dispaly process -01
+print("Hello Python")
+language2 = "Java"
+print("Hello Python", language2, end="@")
+print("Java")
+
+
+# Display Process -02
+language = "Python"
+print("Hello:", language,"and Java")
+
+# P - 03
+
+country = "Bangladesh"
+country2 = "USA"
+
+print("Country one is : {} and Country two is: {}".format(country, country2))
+print("Country one is : {c2} and Country two is: {c1}". format(c1=country, c2=country2))
+
+# P -04
+
+print(f"Country are {country} and {country2}")
+print(f'Country are {country2} and {country}')
+
+# P -05
+
+num = 10.19181843847575
+
+"""
+%f - float
+%d - int
+%s - string
+"""
+print(num)
+
+print("%.2f" % num)
+
+print("%d" % 10)
+
+print("%s" % country)
+
+print("Country are %s and %s" % (country, country2))
+
+
+
+
+
+
